f1e1371bee8514b255a5684eb5873668a908280f
fangwenming <fangwenming@yy.com>
2019-12-30 16:33:54 +0800

    清空项目

--- magerpage/src/main/java/com/yy/magerpage/model/modelenum/ListWidgetType.kt
+++
@@ -1,9 +0,0 @@
-package com.yy.magerpage.model.modelenum
-
-/**
- * Created by Sven on 10/05/2019
- */
-enum class ListWidgetType(val type: String) {
-    SINGLE("single"),
-    DOUBLE("double"),
-}
